0c726efa47b76d16ac02839e0486474230dca621
PaulPrabahar <prabahar1080@gmail.com>
2023-11-11 15:02:15 +0530

    operators

--- 04_operators.py
+++ 04_operators.py
@@ -8,3 +8,33 @@ print(x / y)  # gives decimal value. 2.0
 print(x // y)  # gives floor  value. 2 if 2.5
 print(round(x / y))  # gives floor  value. 3 if 2.5
 print(x ** y)  # gives the power of the values
+
+# comparision operator
+print(x >= y)
+print(x <= y)
+print(x != y)
+print(x == y)
+print(x < y)
+print(x > y)
+
+
+a = True
+b = True
+
+# boolean operator
+print(not a)
+print(a and b)
+print(a or b)
+
+# Let's create a if, else example
+
+name = "paulp"
+
+if name == "paul":
+    print("prabahar")
+else:
+    print("not prabahar")
+
+
+# Ternary Operator
+print("prabahar") if name == "paul" else print("not prabahar")
