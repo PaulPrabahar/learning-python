ce451ee475c4ce54d9d126e4c9a46aba1aa93dbf
PaulPrabahar <prabahar1080@gmail.com>
2023-11-18 16:43:29 +0530

    data types

--- 05_datatypes.py
+++ 05_datatypes.py
@@ -1,6 +1,28 @@
 # String data types.
 # literal assignment.
 first = "paul"
+last = "prabahar"
 print(type(first))
 print(type(first) == str)
 print(isinstance(first, str))
+
+#Concatination of a string.
+fullname = first +" " + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
+#Casting a number into a string
+year = str(2000)
+print("I love music from " + year + "...")
+
+#multiline string
+multilinestr = '''
+hi my name is paul
+                  hi i am essaki
+hey how are you
+                  I am fine.yo.
+
+'''
+print(multilinestr)
