6a6c45cda8c10382fc2976c983a262c0a5b200c9
PaulPrabahar <prabahar1080@gmail.com>
2023-11-20 19:10:48 +0530

    list in python and all it's method

--- 07_listTuples.py
+++ 07_listTuples.py
@@ -1,4 +1,7 @@
 #List
+
+myNewList = list([1,1,2,3,5,8,13])
+names = ['paul', 'jerry', 'alwin','elbin', 'cyril','Anto','Zaiul','Nanako']
 list1 = ["stewie", 11, False]
 list = ['paul', 23, True] #list example.
 emptylist = [] #empty list example.
@@ -19,4 +22,39 @@ print(list)
 list.extend(['peter'])
 print(list)
 
+list.insert(0, "puppy")
+print(list)
+# list[0:0] = ["prasanna"] #To replace the element in the particular position but do not remove. 
+# print(list)
+print(list)
+list[0:1] = ["prasanna"]  #To remove and replace the element form a particular index position.
+print(list)
 
+list.remove('prasanna')
+print(list)
+
+namey = list.pop()
+print(list)
+print(namey)
+
+del list[3]
+print(list)
+
+# list.clear()
+# print(list)
+
+# names.sort()
+# print(names)
+
+# names.sort(key=str.lower) #the items get sorted irrespective of their case. 
+# print(names)
+
+#To make a copy without altering the original list.
+#method 1 to use global method.
+print(sorted(names))
+#method2 to use copy method.
+myNames = names.copy()
+myNames.sort(key=str.lower)
+print(names)
+print(myNames)
+print(myNewList)
